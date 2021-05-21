82d610f34fa495fc37f5324766af558d4c31913c
Guenole76 <54894682+Guenole76@users.noreply.github.com>
2021-05-21 16:34:34 +0200

    bdd

--- projetsymfony/src/Controller/CardController.php
+++ projetsymfony/src/Controller/CardController.php
@@ -5,20 +5,51 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\UberEatType;
+use App\Form\CardType;
+use App\Repository\PokemonRepository;
+use App\Entity\Pokemon;
 
 class CardController extends AbstractController
 {
+
+
+
+    /**
+     * @Route ("/pokemon")
+     */
+    public function PokemonListe()
+    {
+        $cards = $this->getDoctrine()
+            ->getRepository(Pokemon::class)
+            ->findAll();
+
+        return ($this->render('card/pokemon.html.twig',
+        [
+            'cards' => $cards
+        ]));
+    }
 
     /**
      * @Route("/form", name="formulairePost", methods={"POST"})
      */
     public function traitementFormulaire(Request $request): Response
     {
-        $form = $this->createForm(UberEatType::class);
+        $form = $this->createForm(CardType::class);
         $form->handleRequest($request);
 
         $data = $form->getData();
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Pokemon();
+        $product->setNom($data['Nom']);
+        $product->setType($data['Extension']);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
 
         var_dump($data);
         die();
@@ -36,7 +67,7 @@ class CardController extends AbstractController
      */
     public function index(): Response
     {
-        $form = $this->createForm(UberEatType::class);
+        $form = $this->createForm(CardType::class);
         return $this->render('card/index.html.twig', [
             'controller_name' => 'CardController',
             'monFormulaire' => $form->createView(),
